e1bdd0504c4affc605019d57052b658a4577f4d4
Bruno <141152582+BrUn0-P1nh41r0@users.noreply.github.com>
2025-03-04 17:40:59 +0000

    part 1 completed

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
+url = "https://www.billboard.com/charts/hot-100/" + date
+
+response = requests.get(url=url, headers=header)
+soup = BeautifulSoup(response.text, "html.parser")
+soup_title = soup.select("li ul li h3")
+song_names = []
+
+for song in soup_title:
+    song_names.append(song.getText().strip())
+
+print(song_names)
