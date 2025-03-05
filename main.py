5caee52db9f1b2c0fb641c0cf75573529a5379d4
Bruno <141152582+BrUn0-P1nh41r0@users.noreply.github.com>
2025-03-05 17:16:08 +0000

    day 46 completed

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 import requests
 import os
 import spotipy
@@ -11,26 +12,34 @@ SPO_REDIRECT_URL = os.environ["SPO_REDIRECT_URL"]
 AUTH_MANAGER = SpotifyOAuth(client_id=SPO_CLIENT_ID,
                             client_secret=SPO_CLIENT_SECRET,
                             redirect_uri=SPO_REDIRECT_URL,
-                            scope="user-library-read")
+                            scope="playlist-modify-private",
+                            show_dialog=True,
+                            cache_path=".cache")
 
+sp = spotipy.Spotify(auth_manager=AUTH_MANAGER)
+
+user_id = sp.current_user()["id"]
 date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
 header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
 url = "https://www.billboard.com/charts/hot-100/" + date
-
-sp = spotipy.Spotify(auth_manager=AUTH_MANAGER)
 
 response = requests.get(url=url, headers=header)
 soup = BeautifulSoup(response.text, "html.parser")
 soup_title = soup.select("li ul li h3")
 song_names = []
+song_uri = []
 
 for song in soup_title:
     song_names.append(song.getText().strip())
 
-results = sp.current_user_saved_tracks()
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    print(idx, track['artists'][0]['name'], " – ", track['name'])
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uri.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
 
-user = sp.user("11178817830")
-print(user)
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uri)
